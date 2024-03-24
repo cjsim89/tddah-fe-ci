0988c8f3b6649c33782bb07228ebc11e8ea0c6c0
Matt Garcia <MattpGarcia13@users.noreply.github.com>
2024-03-24 13:28:44 -0600

    Adding inital Pages/Layout for Project
    Co-authored-by: Florenzo Bauer <FlorenzoBauer@users.noreply.github.com>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from '../Home/Home'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
 
 function App() {
   return (
-    <div className="App">
-    hi
-    </div>
+    <section className='main'>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </section>
   );
 }
 
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Form from '../Form/Form'
+
+
+function Home() {
+
+
+
+  return (
+    <section>
+      <Navbar />
+      <h1>hi</h1>
+      <Form />
+    </section>
+  )
+}
+
+export default Home
